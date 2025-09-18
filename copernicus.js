292ae729814d3f0b0ed5d2363aa7a690c467dece
James Murdza <james@jamesmurdza.com>
2025-09-18 11:19:10 -0400

    Fix planet sprites

--- copernicus.js
+++ copernicus.js
@@ -25,57 +25,52 @@ function setZoom(z) {
 	document.querySelector("canvas").style.zoom = zoom;
 }
 
-// Planet calculate X position from orbit
-Phaser.Sprite.prototype.calculateX = function (t) {
-	return scale * this.distance * Math.sin(t * Math.PI / timeScale / this.period);
-}
-
-// Planet calculate Y position from orbit
-Phaser.Sprite.prototype.calculateY = function (t) {
-	return scale * this.distance * Math.cos(t * Math.PI / timeScale / this.period);
-}
-
-// Planet update position in game
-Phaser.Sprite.prototype.updatePosition = function () {
-	this.y = (this == centerPlanet) ? 0 : this.calculateX(t) - centerX;
-	this.x = (this == centerPlanet) ? 0 : this.calculateY(t) - centerY;
-
-	// Draw the trail.
-	background.context.fillRect(this.x + size / 2, this.y + size / 2, 1, 1);
-	background.dirty = true;
-}
-
 // Preload
 function preload() {
 	game.load.baseURL = 'http://examples.phaser.io/assets/';
 	game.load.crossOrigin = 'anonymous';
-
-	game.load.image('sun', 'sprites/yellow_ball.png');
-	game.load.image('earth', 'sprites/green_ball.png');
-	game.load.image('moon', 'sprites/blue_ball.png');
 }
 
 // Set up game
 function create() {
 
-	let newPlanet = function(image, distance, period) {
-		let planet = game.add.sprite(distance, 0, image);
-		planet.width = planet.height = 10;
+	let newPlanet = function(color, distance, period) {
+		let planet = game.add.graphics(0, 0);
+		planet.beginFill(color, 1);
+		planet.drawCircle(0, 0, 10);
+		planet.endFill();
 		planet.distance = distance;
 		planet.period = period;
-		planet.anchor.set(0.5);
+		
+		planet.calculateX = function (t) {
+			return scale * this.distance * Math.sin(t * Math.PI / timeScale / this.period);
+		}
+		
+		planet.calculateY = function (t) {
+			return scale * this.distance * Math.cos(t * Math.PI / timeScale / this.period);
+		}
+		
+		planet.updatePosition = function () {
+			this.y = (this == centerPlanet) ? 0 : this.calculateX(t) - centerX;
+			this.x = (this == centerPlanet) ? 0 : this.calculateY(t) - centerY;
+	
+			// Draw the trail.
+			background.context.fillRect(this.x + size / 2, this.y + size / 2, 1, 1);
+			background.dirty = true;
+		}
+		// planet.anchor.set(0.5);
 		return planet;
 	}
 
 	// Planet, radius, period
 	planets = [
-		newPlanet('sun', 0, 1),
-		newPlanet('earth', .39, .24),
-		newPlanet('earth', .72, .62),
-		newPlanet('earth', 1, 1),
-		newPlanet('earth', 1.52, 1.88),
-		newPlanet('earth', 5.20, 11.86),
-		newPlanet('earth', 9.54, 29.46)
+		newPlanet(0xffff00, 0, 1),
+		newPlanet(0xff0000, .39, .24),
+		newPlanet(0xcccccc, .72, .62),
+		newPlanet(0x0000ff, 1, 1),
+		newPlanet(0xff8800, 1.52, 1.88),
+		newPlanet(0x00ff00, 5.20, 11.86),
+		newPlanet(0x8800ff, 9.54, 29.46)
 	];
 
 	background = game.add.bitmapData(game.world.bounds.width, game.world.bounds.height);
